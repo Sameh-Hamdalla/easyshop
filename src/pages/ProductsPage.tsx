8585dc05e8086bd32678292ade7450ef4324a618
Sameh Hamdalla <sa_mh87@outlook.de>
2026-07-10 11:16:32 +0200

    Bilder

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -2,13 +2,17 @@
 
 import { products } from "../data/products";
 import ProductCard from "../components/products/ProductCard";
+import "./ProductsPage.css";
 
 function ProductsPage() {
   return (
-    <main>
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+    <main className="products-page">
+      <h1 className="products-page__title"> Unsere Produkte</h1>
+      <div className="products-page__grid">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </main>
   );
 }
