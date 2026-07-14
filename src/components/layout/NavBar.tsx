88070941d309da2ebdb7e9002f4838a6f8175d6b
Sameh Hamdalla <sa_mh87@outlook.de>
2026-07-14 09:43:13 +0200

    Navbar hunzufügt mit CSS

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,28 +1,35 @@
-
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./NavBar.css";
 
 function NavBar() {
   return (
-        <nav className="navbar">
-          <Link to= "/" className="navbar__logo">
-            EasyShop Hotel
-          </Link>
+    <header>
+      <nav className="navbar">
+        <Link to="/" className="navbar__logo">
+          EasyShop Hotel
+        </Link>
 
-          <ul className="navbar__menu">
-            <li>
-              <Link className="navbar__link"  to="/">Startseite</Link>
-            </li>
+        <ul className="navbar__menu">
+          <li>
+            <Link to="/" className="navbar__link">
+              Startseite
+            </Link>
+          </li>
 
-            <li>
-              <Link className="navbar__link" to="/products">Produkte</Link>
-            </li>
+          <li>
+            <Link to="/products" className="navbar__link">
+              Produkte
+            </Link>
+          </li>
 
-            <li>
-              <Link className="navbar__link" to="/cart">Warenkorb</Link>
-            </li>
-          </ul>
-        </nav>
+          <li>
+            <Link to="/cart" className="navbar__link">
+              Warenkorb
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
